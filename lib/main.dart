0a9b0c54e99597308a2420b357027b8e1cc4b97a
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-23 17:20:47 +0300

    71.Combining Widgets

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Personal Expenses App"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text("Chart"),
+                elevation: 5.0, // gölgelendirme
+              ),
+            ),
+            Card(
+              child: Text("List of tx"),
+            ),
+          ],
+        ));
+  }
+}
