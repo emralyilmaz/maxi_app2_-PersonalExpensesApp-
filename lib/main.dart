dfabc815378ec84cc0df7c0c54e07735b7db3bbb
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-24 14:59:26 +0300

    84. Fetching User Input

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,12 @@ class MyHomePage extends StatelessWidget {
         amount: 16.53,
         date: DateTime.now()),
   ];
+
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +39,7 @@ class MyHomePage extends StatelessWidget {
           title: Text("Personal Expenses App"),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //  mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -42,6 +48,39 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
                 child: Text("Chart"),
                 elevation: 5.0, // gölgelendirme
+              ),
+            ),
+            Card(
+              elevation: 2,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: "Title"),
+                      controller: titleController,
+                      // onChanged: (val) => titleInput = val,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: "Amount"),
+                      controller: amountController,
+                      // onChanged: (val) {
+                      //   amountInput = val;
+                      // },
+                    ),
+                    FlatButton(
+                      child: Text("Add Transaction"),
+                      textColor: Colors.purple,
+                      onPressed: () {
+                        print(titleController.text);
+                        print(amountController.text);
+                        // print(titleInput);
+                        // print(amountInput);
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
             Column(
