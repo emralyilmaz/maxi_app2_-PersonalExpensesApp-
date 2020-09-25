5b8d47ecbf06eaddaa7619a7cf91ac9dc54f674c
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-25 22:43:32 +0300

    88. Making the List Scrollable

--- lib/main.dart
+++ lib/main.dart
@@ -27,20 +27,22 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("Personal Expenses App"),
         ),
-        body: Column(
-          //  mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text("Chart"),
-                elevation: 5.0, // gölgelendirme
+        body: SingleChildScrollView(
+          child: Column(
+            //  mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text("Chart"),
+                  elevation: 5.0, // gölgelendirme
+                ),
               ),
-            ),
-            UserTransactions(),
-          ],
+              UserTransactions(),
+            ],
+          ),
         ));
   }
 }
