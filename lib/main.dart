4e14732fd5ce2b61d575ab4d0fd074e504dd4830
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-27 19:08:36 +0300

    100. Deriving Recent Transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses_app/widgets/chart.dart';
 import 'models/transaction.dart';
 import 'package:personal_expenses_app/widgets/new_transaction.dart';
 import 'package:personal_expenses_app/widgets/transaction_list.dart';
@@ -42,6 +43,13 @@ class _MyHomePageState extends State<MyHomePage> {
     //     amount: 16.53,
     //     date: DateTime.now()),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTrasactions.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
   void _addNewTransaction(String txTitle, double txamount) {
     final newTx = Transaction(
         id: DateTime.now().toString(),
@@ -89,14 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //  mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text("Chart"),
-                elevation: 5.0, // gölgelendirme
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTrasactions),
           ],
         ),
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -13,19 +13,25 @@ class Chart extends StatelessWidget {
         Duration(days: index),
       );
 
-      double totalSum;
+      double totalSum = 0.0;
 
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
-            recentTransactions[i].date.year == weekDay.year) {}
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
       }
-      return {"day": DateFormat.E(weekDay), "amount": 9.99};
+
+      print(DateFormat.E().format(weekDay));
+      print(totalSum);
+      return {"day": DateFormat.E().format(weekDay), "amount": totalSum};
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
