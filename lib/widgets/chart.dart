29e3b8cb903f7d1d75747722416329e07bf49606
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-27 19:57:02 +0300

    101. Creating Bars for our Chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -25,7 +25,10 @@ class Chart extends StatelessWidget {
 
       print(DateFormat.E().format(weekDay));
       print(totalSum);
-      return {"day": DateFormat.E().format(weekDay), "amount": totalSum};
+      return {
+        "day": DateFormat.E().format(weekDay).substring(0, 1),
+        "amount": totalSum
+      };
     });
   }
 
@@ -36,7 +39,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          return Text("${data["day"]}: ${data["amount"]}");
+        }).toList(),
       ),
     );
   }
