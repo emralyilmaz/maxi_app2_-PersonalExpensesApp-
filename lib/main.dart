d53622085263c3c9d9e38ad46495e6918319d6c5
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-27 14:02:06 +0300

    96. Adding Images to the App

--- lib/main.dart
+++ lib/main.dart
@@ -34,13 +34,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTrasactions = [
-    Transaction(
-        id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: "t2",
-        title: "Weekly Groceries",
-        amount: 16.53,
-        date: DateTime.now()),
+    // Transaction(
+    //     id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
+    // Transaction(
+    //     id: "t2",
+    //     title: "Weekly Groceries",
+    //     amount: 16.53,
+    //     date: DateTime.now()),
   ];
   void _addNewTransaction(String txTitle, double txamount) {
     final newTx = Transaction(
