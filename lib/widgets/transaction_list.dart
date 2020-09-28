3fbe7668d2821449382f00b56df2952dc452ddb9
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-28 14:29:09 +0300

    105. Adding a ListTile Widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -26,46 +26,62 @@ class TransactionList extends StatelessWidget {
               ],
             )
           : ListView.builder(
+              itemCount: transactions.length,
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).primaryColor,
-                                width: 2)),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "\$ ${transactions[index].amount.toStringAsFixed(2)}", // how to use "$"!!
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactions[index].title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            // DateFormat("yyyy.MM.dd").format(tx.date),
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      )
-                    ],
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: FittedBox(
+                              child: Text("\$ ${transactions[index].amount}")),
+                        )),
+                    title: Text(transactions[index].title),
+                    subtitle: Text(
+                        DateFormat.yMMMd().format(transactions[index].date)),
                   ),
                 );
+                // return Card(
+                //   child: Row(
+                //     children: [
+                //       Container(
+                //         margin:
+                //             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                //         decoration: BoxDecoration(
+                //             border: Border.all(
+                //                 color: Theme.of(context).primaryColor,
+                //                 width: 2)),
+                //         padding: EdgeInsets.all(10),
+                //         child: Text(
+                //           "\$ ${transactions[index].amount.toStringAsFixed(2)}", // how to use "$"!!
+                //           style: TextStyle(
+                //               fontWeight: FontWeight.bold,
+                //               fontSize: 20,
+                //               color: Theme.of(context).primaryColor),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: [
+                //           Text(
+                //             transactions[index].title,
+                //             style: TextStyle(
+                //                 fontSize: 16, fontWeight: FontWeight.bold),
+                //           ),
+                //           Text(
+                //             DateFormat.yMMMd().format(transactions[index].date),
+                //             // DateFormat("yyyy.MM.dd").format(tx.date),
+                //             style: TextStyle(color: Colors.grey),
+                //           ),
+                //         ],
+                //       )
+                //     ],
+                //   ),
+                // );
               },
-              itemCount: transactions.length,
             ),
     );
   }
