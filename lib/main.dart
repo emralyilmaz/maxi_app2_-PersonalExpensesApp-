7e39aa8f9fc51d756b04ab49c1d20f67afe2b97c
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-10-07 23:06:49 +0300

    131. More Cupertino Styles

--- lib/main.dart
+++ lib/main.dart
@@ -157,7 +157,10 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Show Chart'),
+                Text(
+                  'Show Chart',
+                  style: TextStyle(color: Colors.purple),
+                ),
                 Switch.adaptive(
                   activeColor: Theme.of(context).accentColor,
                   value: _showChart,
