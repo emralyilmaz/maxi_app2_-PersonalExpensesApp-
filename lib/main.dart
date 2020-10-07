55f50758519f43b5e46661e4bb8f24c25e6da227
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-10-07 14:23:35 +0300

    118. Calculating Sizes Dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -88,28 +88,40 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Personal Expenses App",
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              fontFamily: "BalooTammudu2"),
-        ),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () => _startAddNewTransaction(context))
-        ],
+    final appBar = AppBar(
+      title: Text(
+        "Personal Expenses App",
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            fontFamily: "BalooTammudu2"),
       ),
+      actions: [
+        IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context))
+      ],
+    );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           //  mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Chart(_recentTransactions),
-            TransactionList(_userTrasactions, _deleteTransaction),
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      appBar.preferredSize.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.4,
+              child: Chart(_recentTransactions),
+            ),
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                        appBar.preferredSize.height -
+                        MediaQuery.of(context).padding.top) *
+                    0.6,
+                child: TransactionList(_userTrasactions, _deleteTransaction)),
           ],
         ),
       ),
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,84 +10,82 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                Text("No Transactions added yet :)"),
-                SizedBox(height: 20),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    "assets/images/me.png",
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: EdgeInsets.all(6.0),
-                          child: FittedBox(
-                              child: Text("\$ ${transactions[index].amount}")),
-                        )),
-                    title: Text(transactions[index].title),
-                    subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date)),
-                    trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor,
-                        onPressed: () => deleteTx(transactions[index].id)),
-                  ),
-                );
-                // return Card(
-                //   child: Row(
-                //     children: [
-                //       Container(
-                //         margin:
-                //             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                //         decoration: BoxDecoration(
-                //             border: Border.all(
-                //                 color: Theme.of(context).primaryColor,
-                //                 width: 2)),
-                //         padding: EdgeInsets.all(10),
-                //         child: Text(
-                //           "\$ ${transactions[index].amount.toStringAsFixed(2)}", // how to use "$"!!
-                //           style: TextStyle(
-                //               fontWeight: FontWeight.bold,
-                //               fontSize: 20,
-                //               color: Theme.of(context).primaryColor),
-                //         ),
-                //       ),
-                //       Column(
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: [
-                //           Text(
-                //             transactions[index].title,
-                //             style: TextStyle(
-                //                 fontSize: 16, fontWeight: FontWeight.bold),
-                //           ),
-                //           Text(
-                //             DateFormat.yMMMd().format(transactions[index].date),
-                //             // DateFormat("yyyy.MM.dd").format(tx.date),
-                //             style: TextStyle(color: Colors.grey),
-                //           ),
-                //         ],
-                //       )
-                //     ],
-                //   ),
-                // );
-              },
-            ),
-    );
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              Text("No Transactions added yet :)"),
+              SizedBox(height: 20),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  "assets/images/me.png",
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6.0),
+                        child: FittedBox(
+                            child: Text(
+                                "\$ ${transactions[index].amount.toStringAsFixed(2)}")),
+                      )),
+                  title: Text(transactions[index].title),
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => deleteTx(transactions[index].id)),
+                ),
+              );
+              // return Card(
+              //   child: Row(
+              //     children: [
+              //       Container(
+              //         margin:
+              //             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              //         decoration: BoxDecoration(
+              //             border: Border.all(
+              //                 color: Theme.of(context).primaryColor,
+              //                 width: 2)),
+              //         padding: EdgeInsets.all(10),
+              //         child: Text(
+              //           "\$ ${transactions[index].amount.toStringAsFixed(2)}", // how to use "$"!!
+              //           style: TextStyle(
+              //               fontWeight: FontWeight.bold,
+              //               fontSize: 20,
+              //               color: Theme.of(context).primaryColor),
+              //         ),
+              //       ),
+              //       Column(
+              //         crossAxisAlignment: CrossAxisAlignment.start,
+              //         children: [
+              //           Text(
+              //             transactions[index].title,
+              //             style: TextStyle(
+              //                 fontSize: 16, fontWeight: FontWeight.bold),
+              //           ),
+              //           Text(
+              //             DateFormat.yMMMd().format(transactions[index].date),
+              //             // DateFormat("yyyy.MM.dd").format(tx.date),
+              //             style: TextStyle(color: Colors.grey),
+              //           ),
+              //         ],
+              //       )
+              //     ],
+              //   ),
+              // );
+            },
+          );
   }
 }
